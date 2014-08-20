efb6b5f9676b96758e5f8f1a0401e6ed7d575f32
xuzhaocheng <zhaochengxu1991@gmail.com>
2014-08-20 12:30:15 +0800

    Initial commit

---
+++ PhotoViewer/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  PhotoViewer
+//
+//  Created by xuzhaocheng on 14-8-20.
+//  Copyright (c) 2014年 Zhejiang University. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ PhotoViewer/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  PhotoViewer
+//
+//  Created by xuzhaocheng on 14-8-20.
+//  Copyright (c) 2014年 Zhejiang University. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
